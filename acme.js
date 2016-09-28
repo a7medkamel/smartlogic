51a71158ffd6c55bf568b9d3ed672e12c3fd0e49
Ahmed Kamel <ahmed.kamel@docusign.com>
2016-09-28 15:54:10 -0700

    read accounts

--- acme.js
+++ acme.js
@@ -1,13 +1,14 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-var url = `mongodb://acme:password@ds041546.mlab.com:41546/?authSource=hackathon`;
+var url = `mongodb://acme:password@ds041546.mlab.com:41546/hackathon?authSource=hackathon`;
 
 module.exports = function(req, res, next){
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
-    // todo [akamel] read some data
-    res.end();
-    db.close();
+    db.collection('Account').find({}).toArray(function(err, items) {
+      res.send(items);
+      db.close();
+    });
   });
 };
