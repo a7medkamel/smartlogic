1333b828d9b1a8b96b42be22f950c53e15fd232f
Ahmed Kamel <ahmed.kamel@docusign.com>
2016-09-28 16:05:40 -0700

    add opportunity script

--- opportunity.js
+++ opportunity.js
@@ -6,8 +6,13 @@ var url = `mongodb://acme:password@ds041546.mlab.com:41546/hackathon?authSource=
 module.exports = function(req, res, next){
   // Use connect method to connect to the Server
   MongoClient.connect(url, function(err, db) {
-    db.collection('Account').find({ _id : req.query.opportunity_id }).toArray(function(err, items) {
-      res.send(items);
+    db.collection('Opportunity').findOne({ _id : req.query.opportunity_id }, function(err, opportunity) {
+      db.collection('Account').findOne({ _id : opportunity.AccountID }, function(err, account){
+        res.send({
+          opportunity : opportunity,
+          account     : account
+        });
+      });
       db.close();
     });
   });
