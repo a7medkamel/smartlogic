e9e70fa055e1b85fb9d4110c1859984cc7d2367c
Caly Moss <caly.moss@docusign.com>
2016-09-28 18:16:19 -0700

    username

---
+++ userName.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+
+// Connection URL
+var url = `mongodb://acme:password@ds041546.mlab.com:41546/hackathon?authSource=hackathon`;
+
+module.exports = function(req, res, next){
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, function(err, db) {
+    db.collection('Account').findOne({ AccountName : req.query.name }, function(err, account) {
+      res.send({
+        valid: account !== null,
+        message: 'account'
+      });
+      db.close();
+    });
+  });
+};
